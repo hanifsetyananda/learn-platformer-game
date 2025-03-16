2b9819613ba65b984a6a213a088ebe2e1a8f1efd
Hanif Setyananda <hanifsetyananda17@gmail.com>
2025-03-17 05:09:47 +0700

    added gitignore

--- obj/Debug/net8.0/Platformer.AssemblyInfo.cs
+++ obj/Debug/net8.0/Platformer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e183e2dcbdc39330c034e05e6e1f0a3bb7abdaf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
